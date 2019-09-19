1cdfa393df17406794856f2772e39bcec9c3b915
Stormcoder <teryaevs@yandex.ru>
2019-09-19 09:21:38 +0300

    Создание проекта

---
+++ src/main/java/com/rypterium/teryaev_test/TeryaevTestApplication.java
@@ -0,0 +1,13 @@
+package com.rypterium.teryaev_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeryaevTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeryaevTestApplication.class, args);
+	}
+
+}

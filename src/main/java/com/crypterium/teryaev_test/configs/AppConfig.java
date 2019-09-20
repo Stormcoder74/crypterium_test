261aa9e42d20ecd9f28275df69ac8e06793c992c
Stormcoder <teryaevs@yandex.ru>
2019-09-20 12:19:17 +0300

    добавляем настройки аутентификации

---
+++ src/main/java/com/crypterium/teryaev_test/configs/AppConfig.java
@@ -0,0 +1,9 @@
+package com.crypterium.teryaev_test.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.geekbrains.demoboot")
+public class AppConfig {
+}
